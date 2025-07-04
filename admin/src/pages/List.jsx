52bee4d4b6319c19af29393907006e3b2cb0417a
MikhilExplorer <ailani.mikhil@gmail.com>
2025-07-04 15:50:12 +0530

    completed admin ui

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/admin_assets/assets";
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="w-full py-4 px-7 min-h-screen">
+      <h1>All Product List</h1>
+      <div className="hidden sm:grid sm:grid-cols-[1fr_3fr_1fr_1fr_1fr] bg-gray-100 p-2 font-semibold text-sm">
+        <h1>Image</h1>
+        <h1>Name</h1>
+        <h1>Category</h1>
+        <h1>Price</h1>
+        <h1>Action</h1>
+      </div>
+      <div className="grid grid-cols-1 py-3 gap-3">
+        <div className="grid grid-rows-2 grid-cols-[1fr_2fr_1fr] sm:grid-rows-1 sm:grid-cols-[1fr_3fr_1fr_1fr_1fr]  border-1 border-gray-200 p-2 items-center">
+          <div className="w-12 h-15 bg-blue-100"></div>
+          <h1>Kid Tapered Slim Fit Trouser</h1>
+          <h1>Kids</h1>
+          <h1>$38</h1>
+          <h1 className="cursor-pointer">X</h1>
+        </div>
+        <div className="grid grid-rows-2 grid-cols-[1fr_2fr_1fr] sm:grid-rows-1 sm:grid-cols-[1fr_3fr_1fr_1fr_1fr]  border-1 border-gray-200 p-2 items-center">
+          <div className="w-12 h-15 bg-blue-100"></div>
+          <h1>Kid Tapered Slim Fit Trouser</h1>
+          <h1>Kids</h1>
+          <h1>$38</h1>
+          <h1 className="cursor-pointer">X</h1>
+        </div>
+        <div className="grid grid-rows-2 grid-cols-[1fr_2fr_1fr] sm:grid-rows-1 sm:grid-cols-[1fr_3fr_1fr_1fr_1fr]  border-1 border-gray-200 p-2 items-center">
+          <div className="w-12 h-15 bg-blue-100"></div>
+          <h1>Kid Tapered Slim Fit Trouser</h1>
+          <h1>Kids</h1>
+          <h1>$38</h1>
+          <h1 className="cursor-pointer">X</h1>
+        </div>
+        <div className="grid grid-rows-2 grid-cols-[1fr_2fr_1fr] sm:grid-rows-1 sm:grid-cols-[1fr_3fr_1fr_1fr_1fr]  border-1 border-gray-200 p-2 items-center">
+          <div className="w-12 h-15 bg-blue-100"></div>
+          <h1>Kid Tapered Slim Fit Trouser</h1>
+          <h1>Kids</h1>
+          <h1>$38</h1>
+          <h1 className="cursor-pointer">X</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/admin_assets/assets";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="w-full flex flex-col gap-3 px-3  sm:px-12 py-8 text-gray-500">
+      <h1>Order Page</h1>
+      <div className="text-sm flex flex-col gap-2 lg:text-base lg:grid lg:grid-rows-1 lg:grid-cols-[1fr_3fr_2fr_1fr_2fr] border-1 border-gray-200 px-6 py-6">
+        <img className="w-15" src={assets.parcel_icon} alt="" />
+        <div className="flex flex-col gap-3">
+          <h1>Kid Tapered Slim Fit Trouser x 1 M</h1>
+          <h1>Rana Dutta</h1>
+          <h1>
+            VILL - SIRKABAD, P.O- SIRKABAD, P.S - ARSHA, Purulia, West Bengal,
+            India, 723154
+          </h1>
+        </div>
+        <div>
+          <h1 className="mb-3">Items : 1</h1>
+          <h1>Method : COD</h1>
+          <h1>Payment : Pending</h1>
+          <h1>Date : 7/4/2025</h1>
+        </div>
+        <h1>$48</h1>
+        <select className="border-1 border-gray-300 p-2 w-fit h-fit outline-none" name="" id="">
+          <option value="">Order Placed</option>
+          <option value="">Packing</option>
+          <option value="">Shipping</option>
+          <option value="">Out for Delivery</option>
+          <option value="">Delivered</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
